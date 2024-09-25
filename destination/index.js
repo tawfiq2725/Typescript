2ecff37f9358b269daf74fa455d566b85329e5e3
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-09-25 13:00:36 +0530

    basics

---
+++ destination/index.js
@@ -0,0 +1,25 @@
+"use strict";
+// Datatypes -primitive
+// Explicit
+// String
+let Emp_name = 'Tawfiq';
+// Integer
+let Emp_id = 100;
+// Boolean
+let Emp_Permanent = true;
+console.log(`Employee Name is ${Emp_name} and Employee ID is ${Emp_id} and Employee is Permanent --> ${Emp_Permanent}`);
+// Array
+let myArray = [1, 2, 3, 4, 5];
+let strArray = ['a', 'b', 'c'];
+let booleanArr = [true, false];
+/*-----------------------------------------------------------------------------------*/
+// Implicit
+// String
+let Empq_name = 'Tawfiq';
+// Integer
+let Empq_id = 100;
+// Boolean
+let Empq_Permanent = true;
+let myArr = [1, 2, 3, 4, 5];
+let strArr = ['a', 'b', 'c'];
+let booleanAr = [true, false];
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+// Datatypes -primitive
+
+// Explicit
+
+// String
+let Emp_name:string = 'Tawfiq';
+
+// Integer
+let Emp_id:number = 100;
+
+// Boolean
+let Emp_Permanent:boolean = true;
+
+console.log(`Employee Name is ${Emp_name} and Employee ID is ${Emp_id} and Employee is Permanent --> ${Emp_Permanent}`)
+
+
+// Array
+let myArray:number[] = [1,2,3,4,5]
+let strArray:string[] = ['a','b','c']
+let booleanArr:boolean[] = [true,false]
+
+/*-----------------------------------------------------------------------------------*/
+
+// Implicit
+
+// String
+let Empq_name= 'Tawfiq';
+// Integer
+let Empq_id = 100;
+// Boolean
+let Empq_Permanent = true;
+
+let myArr = [1,2,3,4,5]
+let strArr = ['a','b','c']
+let booleanAr = [true,false]
